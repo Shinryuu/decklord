89ec50ae07bfeb3cd95584e52c4696c112fd339f
Timmy <76251049+Shinryuu@users.noreply.github.com>
2025-02-27 14:57:52 +0200

    Update deckified.sh

--- deckified.sh
+++ deckified.sh
@@ -6,11 +6,12 @@
 
 # Tweaks
 # Add "rcutree.enable_rcu_lazy=1" to the /etc/sdboot-manage.conf
+# Check /etc/default/scx configuration and make sure to use "LAVD"
 # echo kernel.split_lock_mitigate=0 > /etc/sysctl.d/99-splitlock.conf
 
 # Package list
 SYSTEM_PACKAGES=(
-    ark asusctl bluedevil breeze-gtk dolphin firefox kate kpipewire kscreen
+    acpi_call ark asusctl bluedevil breeze-gtk dolphin firefox kate konsole kpipewire kscreen
     kwayland kwin libkscreen linux-cachyos-deckify linux-cachyos-deckify-headers
     maliit-framework maliit-keyboard mkinitcpio-firmware mpv okular plasma-desktop
     plasma-nm plasma-pa polkit-kde-agent qt-wayland rog-control-center ryzen_smu-dkms-git
@@ -34,4 +35,4 @@ pacman -Qi inputplumber &>/dev/null && sudo pacman -Rns --noconfirm inputplumber
 echo "blacklist hid-asus" | sudo tee /etc/modprobe.d/blacklist.conf > /dev/null
 
 # Deckify the system
-pacman -S --needed --noconfirm cachyos-handheld
+pacman -S --needed --noconfirm cachyos-handheld linux-cachyos-deckify linux-cachyos-deckify-headers
